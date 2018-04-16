a172a31aed53be9d62352d4de2839b37cf0d9eed
CYNOMAC007 <cynomac007@cynomac007s-imac.local>
2018-04-16 15:22:03 +0530

    Testing Commit

--- TestSubodhGit/ViewController.swift
+++ TestSubodhGit/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let message = "Hi Git, Subodh here"
+        
+        print(message)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
